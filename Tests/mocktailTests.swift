3110659538a946f0e5f782abb13c0924710c3f06
Cristian Rojas <cristian.rojas@live.fr>
2025-05-04 23:16:25 +0200

    Deliver 404 on missing resource

--- Tests/mocktailTests.swift
+++ Tests/mocktailTests.swift
@@ -3,11 +3,22 @@
 import XCTest
 
 struct Parser {
+    let resources: [String]
     func parse(_ request: Request) -> Response {
-        Response(statusCode: 400)
+        guard let _ = request.headers.components(separatedBy: " ").get(at: 2) else {
+            return Response(statusCode: 400)
+        }
+        
+        return Response(statusCode: 404)
     }
 }
 
+fileprivate extension Array {
+    func get(at index: Int) -> Element? {
+        guard indices.contains(index) else { return nil }
+        return self[index]
+    }
+}
 struct Response {
     let statusCode: Int
 }
@@ -18,16 +29,30 @@ struct Request {
 
 final class Tests: XCTestCase {
     func test_parser_delivers400ResponseOnEmptyHeaders() {
-        let sut = Parser()
+        let sut = makeSUT()
         let request = Request(headers: "")
         let response = sut.parse(request)
         XCTAssertEqual(response.statusCode, 400)
     }
     
     func test_parser_delivers400OnMalformedHeaders() {
-        let sut = Parser()
+        let sut = makeSUT()
         let request = Request(headers: "GETHTTP/1.1")
         let response = sut.parse(request)
         XCTAssertEqual(response.statusCode, 400)
+    }
+    
+    func test_parser_delivers404OnNonExistentResource() {
+        let sut = makeSUT()
+        let request = Request(headers: "GET /recipes HTTP/1.1")
+        let response = sut.parse(request)
+        XCTAssertEqual(response.statusCode, 404)
+    }
+}
+
+// MARK: - Helpers
+private extension Tests {
+    func makeSUT(resources: [String] = []) -> Parser {
+        Parser(resources: resources)
     }
 }
