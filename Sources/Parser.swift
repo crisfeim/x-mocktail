0660e6c7bc5ef680c34d9f7e40484e6159a20a58
Cristian Rojas <cristian.rojas@live.fr>
2025-05-05 20:28:03 +0200

    Move `Router`to its own file

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -1,86 +1,7 @@
 // Created by Cristian Felipe Patiño Rojas on 5/5/25.
 import Foundation
 
-struct Router {
-    let request: Request
-    let collections: [String: JSON]
-    func handleRequest() -> Response? {
-        switch request.method() {
-        case .GET: return handleGET()
-        case .DELETE: return handleDELETE()
-        case .PUT: return handlePUT()
-        default: return nil
-        }
-    }
-    
-    private func handleGET() -> Response? {
-        switch request.route() {
-        case let .collection(name) where !collectionExists(name):
-            return Response(statusCode: 404)
-        case let .collection(name) where collectionIsEmpty(name):
-            return Response(
-                statusCode: 200,
-                rawBody: "[]",
-                contentLength: "[]".contentLenght()
-            )
-        case let .collection(name) where !collectionIsEmpty(name):
-            let body = collections[name].flatMap(JSONUtils.jsonToString)
-            return Response(
-                statusCode: 200,
-                rawBody: body,
-                contentLength: body?.contentLenght()
-            )
-        case let .resource(item) where itemExists(item):
-            let body = (collections[item.collectionName] as? JSONArray)?.getItem(with: item.id).flatMap(JSONUtils.jsonItemToString)
-            return Response(
-                statusCode: 200,
-                rawBody: body,
-                contentLength: body?.contentLenght()
-            )
-        default: return nil
-        }
-    }
-    
-    private func handleDELETE() -> Response? {
-        switch request.route() {
-        case .collection, .nestedSubroute:
-            return Response(statusCode: 400)
-        case let .resource(item) where !itemExists(item):
-            return Response(statusCode: 404)
-        case .resource:
-            return Response(statusCode: 204)
-        }
-    }
-    
-    private func handlePUT() -> Response? {
-        switch request.route() {
-        case let .resource(item) where !itemExists(item):
-            return Response(statusCode: 200)
-        case .resource where request.body == nil:
-            return Response(statusCode: 400)
-        case .resource where JSONUtils.isValidJSON(request.body!):
-            return Response(
-                statusCode: 200,
-                rawBody:  request.body,
-                contentLength: request.body?.contentLenght()
-            )
-        default: return nil
-        }
-    }
 
-    private func collectionExists(_ collectionName: String) -> Bool {
-        collections.keys.contains(collectionName)
-    }
-    
-    private func collectionIsEmpty(_ collectionName: String) -> Bool {
-        let collection = collections[collectionName] as? JSONArray
-        return collection?.isEmpty ?? true
-    }
-    
-    private func itemExists(_ item: (id: String, collectionName: String)) -> Bool {
-        (collections[item.collectionName] as? JSONArray)?.getItem(with: item.id) != nil
-    }
-}
 
 struct HeadersValidator {
     
@@ -432,20 +353,20 @@ extension Request {
 }
 
 
-private extension String {
+extension String {
     func contentLenght() -> Int {
         data(using: .utf8)?.count ?? count
     }
     
-    func removingBreaklines() -> String {
+    fileprivate func removingBreaklines() -> String {
         self.replacingOccurrences(of: "\n", with: "")
     }
     
-    func removingSpaces() -> String {
+    fileprivate func removingSpaces() -> String {
         self.replacingOccurrences(of: " ", with: "")
     }
     
-    func trimInitialAndLastSlashes() -> String {
+    fileprivate func trimInitialAndLastSlashes() -> String {
         var copy = self
         if copy.first == "/" {
             copy.removeFirst()
