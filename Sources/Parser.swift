303e7704900fdd41594e5dd7b68e8d70d613951d
Cristian Rojas <cristian.rojas@live.fr>
2025-05-05 20:36:01 +0200

    Move `Response.init` to its own file

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -146,35 +146,8 @@ extension Parser {
     }
 }
 
-public extension Response {
-    init(
-        statusCode: Int,
-        rawBody: String? = nil,
-        contentLength: Int? = nil
-    ) {
-        let date = Self.dateFormatter.string(from: Date())
-        
-        let headers = [
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
-            "Access-Control-Allow-Headers": "content-type",
-            "Content-Type": "application/json",
-            "Date": date,
-            "Connection": "close",
-            "Content-Length": contentLength?.description
-        ].compactMapValues { $0 }
-        
-        self.init(statusCode: statusCode, headers: headers, rawBody: rawBody)
-    }
-    
-    static let dateFormatter = DateFormatter() * { df in
-        df.dateFormat = "EEE',' dd MMM yyyy HH:mm:ss zzz"
-        df.locale = Locale(identifier: "en_US_POSIX")
-        df.timeZone = TimeZone(secondsFromGMT: 0)
-    }
-}
 
-private func *<T>(lhs: T, rhs: (inout T) -> Void) -> T {
+func *<T>(lhs: T, rhs: (inout T) -> Void) -> T {
     var copy = lhs
     rhs(&copy)
     return copy
