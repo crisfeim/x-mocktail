82ec012120467bff1264a6fef1e9a34c8d0ddaf9
Cristian Rojas <cristian.rojas@live.fr>
2025-05-05 20:28:31 +0200

    Move `HeadersValidator` to its own file

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -1,57 +1,6 @@
 // Created by Cristian Felipe Patiño Rojas on 5/5/25.
 import Foundation
 
-
-
-struct HeadersValidator {
-    
-    enum ValidationError: Int, Swift.Error {
-        case notFound = 404
-        case badRequest = 400
-        case unsupportedMethod = 405
-        case missingOrWrongMediaType = 415
-    }
-    
-    let request: Request
-    let collections: [String: JSON]
-    
-    typealias Result = Swift.Result<Void, ValidationError>
-    
-    var result: Result {
-        guard request.headers.contains("Host"), let collectionName = request.collectionName()  else {
-            return .failure(.badRequest)
-        }
-        
-        guard let _ = request.method() else {
-            return .failure(.unsupportedMethod)
-        }
-        
-        if request.payloadRequiredRequest() {
-            guard let contentType = request.contentType(), contentType == "application/json" else {
-                return .failure(.missingOrWrongMediaType)
-            }
-        }
-        
-        if [Request.HTTPVerb.DELETE, .PATCH].contains(request.method()) {
-            
-            guard let id = request.route().id else {
-                return .failure(.badRequest)
-            }
-               guard let _ = getItem(withId: id, on: collectionName, collections: collections) else {
-             return .failure(.notFound)
-         }
-        }
-        
-        return .success(())
-    }
-    
-    func getItem(withId id: String, on collectionName: String, collections: [String: JSON]) -> JSONItem? {
-        let items = collections[collectionName] as? JSONArray
-        let item = items?.getItem(with: id)
-        return item
-    }
-}
-
 public struct Parser {
     private let resources: [String: JSON]
     
