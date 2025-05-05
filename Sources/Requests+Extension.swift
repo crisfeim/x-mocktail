8da9e5e5c20d203660f6677e062dee4ea8b342fa
Cristian Rojas <cristian.rojas@live.fr>
2025-05-05 20:34:03 +0200

    Move `Request` helpers to their own file

---
+++ Sources/Requests+Extension.swift
@@ -0,0 +1,89 @@
+// Created by Cristian Felipe Patiño Rojas on 5/5/25.
+
+
+import Foundation
+
+extension Request {
+    func normalizedURL() -> String? {
+        requestHeaders().first?
+            .components(separatedBy: " ")
+            .get(at: 1)?
+            .trimInitialAndLastSlashes()
+    }
+    
+    func requestHeaders() -> [String] {
+        headers.components(separatedBy: "\n")
+    }
+    
+    func urlComponents() -> [String] {
+        Array(normalizedURL()?.components(separatedBy: "/") ?? [])
+    }
+    
+    func id() -> String? {
+        urlComponents().get(at: 1)
+    }
+    
+    func collectionName() -> String? {
+        urlComponents().first
+    }
+    
+    enum HTTPVerb: String {
+        case GET
+        case POST
+        case DELETE
+        case PUT
+        case PATCH
+    }
+    
+    func method() -> HTTPVerb? {
+        guard let verb = requestHeaders().first?.components(separatedBy: " ").first else {
+            return nil
+        }
+        return HTTPVerb(rawValue: verb)
+    }
+    
+    func payloadRequiredRequest() -> Bool {
+        [HTTPVerb.PUT, .PATCH, .POST].contains(method())
+    }
+    
+    func allItems() -> Bool {
+        urlComponents().count == 1
+    }
+    
+    enum RequestType {
+        typealias Item = (id: String, collectionName: String)
+        case collection(name: String)
+        case resource(Item)
+        case nestedSubroute
+        
+        init(_ urlComponents: [String]) {
+            switch urlComponents.count {
+            case 1: self = .collection(name: urlComponents[0])
+            case 2: self = .resource((id: urlComponents[1], collectionName: urlComponents[0]))
+            default: self = .nestedSubroute
+            }
+        }
+        
+        var id: String? {
+            if case let .resource(item) = self {
+                return item.id
+            }
+            return nil
+        }
+    }
+    
+    func route() -> RequestType {
+        RequestType(urlComponents())
+    }
+    
+    func contentType() -> String? {
+        for line in requestHeaders() {
+            if line.lowercased().starts(with: "content-type:") {
+                return line
+                    .dropFirst("content-type:".count)
+                    .trimmingCharacters(in: .whitespaces)
+            }
+        }
+        return nil
+    }
+}
